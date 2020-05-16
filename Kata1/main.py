2ec5c43d35ae60a28f49f28876e2774191f64a86
Leire Maceda <leire.maceda@gmail.com>
2020-05-16 12:39:05 +0200

    terminada la kata 1

--- Kata1/main.py
+++ Kata1/main.py
@@ -1,4 +1,11 @@
-def mitad(numero):
-    return numero / 2
+precio =3.49
+descuento = 60
+precio_con_descuento = precio - (precio * descuento / 100)
 
-print(mitad(12))
+print(precio_con_descuento)
+numero_de_barras = input("introduce el numero de barras vendidas: ")
+numero_de_barras = int(numero_de_barras)
+print("precio habitual:"+str(precio))
+print("precio con descuento:"+ str(precio_con_descuento) )
+print("coste final:" + str(numero_de_barras*precio_con_descuento))
+
