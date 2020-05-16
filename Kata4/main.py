1e8d133d2e801c8e096a01dc658074bdbe50fbaf
Leire Maceda <leire.maceda@gmail.com>
2020-05-16 17:34:33 +0200

    pygame

--- Kata4/main.py
+++ Kata4/main.py
@@ -10,12 +10,22 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 def mover_rectangulo():
+    global speed
     if rectangulo.top + 50 < screen_height:
-        rectangulo.top += 3
+        rectangulo.top += speed
 
 rectangulo = pygame.Rect(10,10,50,50)
+speed = 0
 while True:
     screen.fill(light_gray)
+for event in pygame.event.get():
+    if.event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            speed = 3
+        if event.key == pygame.K_DOWN:
+            speed = 3
+    
+
     mover_rectangulo()
     pygame.draw.rect(screen, white_color, rectangulo)
     pygame.display.flip()
