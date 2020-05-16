658513f4a22685a2c20550041245e833ddaffe6b
Leire Maceda <leire.maceda@gmail.com>
2020-05-16 14:31:45 +0200

    mi bot terminado

--- Kata3/main.py
+++ Kata3/main.py
@@ -4,13 +4,20 @@ from telegram.ext import Updater, CommandHandler
 def main():
     updater =  Updater(token="1289905622:AAHuHtP3Pvc3jTbOu5pbHwhfu9VfJAPdGJM", use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("repite", repite))
+    updater.dispatcher.add_handler(CommandHandler("suma", suma))
     updater.start_polling()
     updater.idle()
 
 def start(update, context):
     update.message.reply_text("Hola, soy un bot")
-    pass
 
+def repite(update, context):
+    update.message.reply_text(update.message.text)
+
+def suma(update, context):
+    resultado = int(context.args[0]) + int (context.args[1])
+    update.message.reply_text ("El resultado es " + str (resultado))
 
 main()
 
