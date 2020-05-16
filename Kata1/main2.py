d23dbd2ce05aa9c69f5e7ed37073a466cabeffa3
Leire Maceda <leire.maceda@gmail.com>
2020-05-16 12:50:19 +0200

    paswords

---
+++ Kata1/main2.py
@@ -0,0 +1,8 @@
+pasword = "contraseña"
+pasword_del_usuario = input("introduzca la  contraseña:")
+
+if pasword == pasword_del_usuario:
+    print("El pasword es correcto")
+else:
+    print("el pasword no coincide")
+    
