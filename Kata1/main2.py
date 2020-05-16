4df0cf212fbd946780060f584010c45b337a5b38
Leire Maceda <leire.maceda@gmail.com>
2020-05-16 13:10:36 +0200

    otro python

--- Kata1/main2.py
+++ Kata1/main2.py
@@ -1,6 +1,6 @@
 pasword = "contraseña"
 pasword_del_usuario = input("introduzca la  contraseña:")
-
+pasword_del_usuario = pasword_del_usuario.lower()
 if pasword == pasword_del_usuario:
     print("El pasword es correcto")
 else:
---
+++ Kata1/main3.py
@@ -0,0 +1,43 @@
+edad = input("introduce tu edad:")
+edad = int(edad)
+
+if edad < 4:
+    print(" El recio de la entrada es 0 euros")
+elif edad >=4 and edad <= 18: 
+    print("El precio de la entrada es de 5 euros")
+else:
+    print("el precio es 10 euros")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
